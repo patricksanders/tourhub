d59f9be92542b342338d092355455494ba47f915
Bipol Alam <bipolalam@gmail.com>
2015-03-05 22:47:20 -0600

    get mbid functionality

---
+++ tourtools/artists.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+url_base = "http://api.setlist.fm/rest/0.1/"
+client = requests.session()
+
+def get_mbid(artist_name):
+    query = {"artistName":artist_name}
+    res_id="search/artists.json"
+    url = "".join( [url_base, res_id] )
+    r = client.get(url, params=query)
+    j = json.loads(r.text)
+    return j['artists']['artist']
+
+
+    
