0514e37d243695cb3660bbfc809d128147a01726
patricksanders <mail@patricksanders.net>
2015-03-16 23:15:34 -0500

    ugh i'm so sorry

--- tourtools/artists.py
+++ tourtools/artists.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import math
 
 url_base = "http://api.setlist.fm/rest/0.1/"
 client = requests.session()
@@ -14,4 +15,34 @@ def get_mbid(artist_name):
     j = { element['@name'] : element['@mbid'] for element in j }
     return j
 
+def get_setlists(mbid):
+    finished = False
+    page = 1
+    j = json.loads('[]')
+    while not finished:
+        setlists, finished = setlist_query(mbid, page)
+        
+        j.append(setlists)
+        page += 1
+
+    return j
+
+def setlist_query(mbid, page):
+    res_id = "artist/" + mbid  + "/setlists.json?p=" + str(page)
+    url = "".join( [url_base, res_id] )
+    r = client.get(url)
+    j = json.loads(r.text)
+
+    page = int(j['setlists']['@page'])
+    total_items = float(j['setlists']['@total'])
+    items_per_page = float(j['setlists']['@itemsPerPage'])
+
+    j = j['setlists']['setlist']
+    if page < math.ceil(total_items / items_per_page):
+        done = False
+    else:
+        done = True
+    j = { element['@id'] : element['sets'] for element in j }
+    return j, done
+    
     
